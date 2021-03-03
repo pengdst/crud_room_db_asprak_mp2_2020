77a6c2226bd13c19c2b8651a8dbff50ee9c138d0
Pengkuh Dwi Septiandi <penxkuh@gmail.com>
2021-03-03 22:08:47 +0700

    membuat activity tambah ubah matkul

---
+++ app/src/main/java/com/mmnuradityo/crud_room_db/ui/activity/TambahUbahMatkulActivity.java
@@ -0,0 +1,16 @@
+package com.mmnuradityo.crud_room_db.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mmnuradityo.crud_room_db.R;
+
+public class TambahUbahMatkulActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_ubah_matkul);
+    }
+}
