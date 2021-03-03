11d05aeada834449cbda321c7efe80050318fa21
Pengkuh Dwi Septiandi <penxkuh@gmail.com>
2021-03-03 21:58:28 +0700

    tambah Matkul dan MahasiswaDao ke AppDataBase.java

--- app/src/main/java/com/mmnuradityo/crud_room_db/database/AppDataBase.java
+++ app/src/main/java/com/mmnuradityo/crud_room_db/database/AppDataBase.java
@@ -3,14 +3,20 @@ package com.mmnuradityo.crud_room_db.database;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import com.mmnuradityo.crud_room_db.database.dao.MahasiswaDao;
+import com.mmnuradityo.crud_room_db.database.dao.MatkulDao;
+import com.mmnuradityo.crud_room_db.database.models.Mahasiswa;
+import com.mmnuradityo.crud_room_db.database.models.Matkul;
+
 /**
  * Created on : 07/11/20
  * Author     : mmnuradityo
  * GitHub     : https://github.com/mmnuradityo
  */
-@Database(entities = {Mahasiswa.class}, version = 2)
+@Database(entities = {Mahasiswa.class, Matkul.class}, version = 3)
 public abstract class AppDataBase extends RoomDatabase {
 
     public abstract MahasiswaDao mahasiswaDao();
+    public abstract MatkulDao matkulDao();
 
 }
