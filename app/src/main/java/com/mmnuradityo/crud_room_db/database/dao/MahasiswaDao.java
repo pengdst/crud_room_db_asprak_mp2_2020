6f077b5e5688d5c25027c536ada6bb9a7be27f82
Pengkuh Dwi Septiandi <penxkuh@gmail.com>
2021-03-03 21:50:14 +0700

    memindahkan MahasiswaDao ke package dao

--- app/src/main/java/com/mmnuradityo/crud_room_db/database/dao/MahasiswaDao.java
+++ app/src/main/java/com/mmnuradityo/crud_room_db/database/dao/MahasiswaDao.java
@@ -1,10 +1,12 @@
-package com.mmnuradityo.crud_room_db.database;
+package com.mmnuradityo.crud_room_db.database.dao;
 
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
+
+import com.mmnuradityo.crud_room_db.database.models.Mahasiswa;
 
 import java.util.List;
 
