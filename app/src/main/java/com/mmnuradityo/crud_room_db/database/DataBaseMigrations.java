aa6bc0d8a86ea139513df00e1c8e59e6dba865fe
Pengkuh Dwi Septiandi <penxkuh@gmail.com>
2021-03-03 21:58:51 +0700

    database migrations create table matkul

--- app/src/main/java/com/mmnuradityo/crud_room_db/database/DataBaseMigrations.java
+++ app/src/main/java/com/mmnuradityo/crud_room_db/database/DataBaseMigrations.java
@@ -17,4 +17,11 @@ public class DataBaseMigrations {
         }
     };
 
+    public static final Migration MIGRATION_2_TO_3 = new Migration(1, 2) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            database.execSQL("CREATE TABLE IF NOT EXISTS matkul (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `nama` TEXT, `sks` TEXT, `semester` TEXT)");
+        }
+    };
+
 }
