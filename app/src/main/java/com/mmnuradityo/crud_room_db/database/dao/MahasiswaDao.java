d57147c206d0652510ab204350d2ad9198db7f01
Pengkuh Dwi Septiandi <penxkuh@gmail.com>
2021-03-04 08:27:19 +0700

    get matkul by Mahasiswa

--- app/src/main/java/com/mmnuradityo/crud_room_db/database/dao/MahasiswaDao.java
+++ app/src/main/java/com/mmnuradityo/crud_room_db/database/dao/MahasiswaDao.java
@@ -20,6 +20,11 @@ public interface MahasiswaDao {
     @Query("SELECT * FROM mahasiswa")
     List<Mahasiswa> getAll();
 
+    @Query("SELECT * FROM matkul " +
+            "JOIN mahasiswamatkulcrossref mref ON mref.matkulId = matkul.id " +
+            "WHERE mref.mahasiswaId LIKE :id")
+    List<Matkul> getMatkulByMahasiswa(int id);
+
     @Query("SELECT * FROM mahasiswa WHERE id LIKE :mahasiswaId LIMIT 1")
     Mahasiswa findById(int mahasiswaId);
 
