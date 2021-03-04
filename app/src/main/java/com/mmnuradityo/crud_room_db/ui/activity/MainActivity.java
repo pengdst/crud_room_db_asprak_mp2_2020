6b25b8720200533e7af0866bc917ba023ef3dadd
Pengkuh Dwi Septiandi <penxkuh@gmail.com>
2021-03-04 08:16:11 +0700

    merubah MainActivity menjadi menu utama

--- app/src/main/java/com/mmnuradityo/crud_room_db/ui/activity/MainActivity.java
+++ app/src/main/java/com/mmnuradityo/crud_room_db/ui/activity/MainActivity.java
@@ -1,59 +1,41 @@
 package com.mmnuradityo.crud_room_db.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.mmnuradityo.crud_room_db.CrudRoomApp;
-import com.mmnuradityo.crud_room_db.ui.adapter.RvAdapter;
-import com.mmnuradityo.crud_room_db.ui.common.DataListListener;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.mmnuradityo.crud_room_db.R;
-import com.mmnuradityo.crud_room_db.database.models.Mahasiswa;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView rvListMahasiswa;
-    private FloatingActionButton fabTambah;
-    private RvAdapter adapter;
+    private Button btnMahasiswa;
+    private Button btnMatkul;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        adapter = new RvAdapter();
 
-        rvListMahasiswa = findViewById(R.id.rv_list_mahasiswa);
-        fabTambah = findViewById(R.id.fab_tambah_data);
+        btnMahasiswa = findViewById(R.id.btn_mahasiswa);
+        btnMatkul = findViewById(R.id.btn_matkul);
 
-        rvListMahasiswa.setAdapter(adapter);
-
-        adapter.setRemoveListener(new DataListListener() {
-            @Override
-            public void onRemoveClick(Mahasiswa mahasiswa) {
-                adapter.removeData(mahasiswa);
-            }
-        });
-
-        fabTambah.setOnClickListener(new View.OnClickListener() {
+        btnMahasiswa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, TambahDanUbahMahasiswaActivity.class));
+                Intent intentMain = new Intent(getApplicationContext(), ListMahasiswaActivity.class);
+                startActivity(intentMain);
             }
         });
 
+        btnMatkul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentMatkul = new Intent(getApplicationContext(), ListMatkulActivity.class);
+                startActivity(intentMatkul);
+            }
+        });
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        List<Mahasiswa> datas = CrudRoomApp.getInstance().getDataBase().mahasiswaDao().getAll();
-        adapter.setData(datas);
-    }
-
 }
